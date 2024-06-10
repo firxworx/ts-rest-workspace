a41a2848f9106ccc4a15ca9ded200ca3d0551c8a
Kevin Firko <kfirko@bitcurve.com>
2024-06-09 20:54:17 -0400

    feat(fastify-api): working vitest for fastify

--- apps/fastify-api/test/app.spec.ts
+++ apps/fastify-api/test/app.spec.ts
@@ -1,20 +1,46 @@
 import Fastify, { type FastifyInstance } from 'fastify'
+import { afterAll, beforeAll, test, it, expect, describe } from 'vitest'
 import { app } from '../src/app/app.js'
+import supertest from 'supertest'
 
-describe('GET /', () => {
+/**
+ * Run this test file:
+ *
+ * ```ts
+ * pnpm --filter fastify-api test test/app.spec.ts
+ * ```
+ *
+ * @see https://github.com/vitest-dev/vitest/blob/main/examples/fastify/test/app.test.ts
+ */
+describe('fastify-api', () => {
   let server: FastifyInstance
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     server = Fastify()
     await server.register(app)
   })
 
-  it('should respond with a message', async () => {
-    const response = await server.inject({
-      method: 'GET',
-      url: '/',
+  afterAll(async () => {
+    await server.close()
+  })
+
+  describe('GET /', () => {
+    // test with http injection
+    it('should respond with a message', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      })
+
+      expect(response.json()).toEqual({ message: 'Hello API' })
     })
 
-    expect(response.json()).toEqual({ message: 'Hello API' })
+    test('with a running server', async () => {
+      await server.ready()
+      const response = await supertest(server.server).get('/').expect(200)
+      const expectedResponseBody = { message: 'Hello API' }
+
+      expect(response.body).toStrictEqual(expectedResponseBody)
+    })
   })
 })
